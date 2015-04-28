fb8d3e5fd12e072e678210752140c34b0254f217
byjok <rkovalchuk@cognince.com>
2015-04-28 17:04:03 +0300

    solved s1c3 started s2c4

--- xor_decrypt.rb
+++ xor_decrypt.rb
@@ -2,18 +2,24 @@ puts "Enter string to decrypt"
 str = gets.chomp
 
 def decrypt_xor(str)
-english_words = ['the','be','to','of','and','a','in','that','have','I','it','for','not','on','with','he','as','you','do','at']
-
-('a'..'z').each do |letter|
-    decoded = str.to_i(16)^letter.to_i(16)
-    puts decoded
-    string_decoded =  decoded.to_s(32)
+english_words = ['the','be','to','of','and','in','that','have','it','for','not','on','with','he','as','you','do','at','my','ice','baby']
+chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+chars.each do |letter|
+    #	puts str.to_i(16)
+    #    puts letter.unpack("H*")
+        code = letter.unpack("H*")[0]*(str.length / 2)
+    #	puts code
+    #	puts code.to_i(16)
+        decoded = str.to_i(16)^code.to_i(16)
+   # puts decoded
+    string_decoded = [decoded.to_s(16)].pack("H*")
+   # puts string_decoded
     english_words.each do |word|
-        if string_decoded.include? word
+        if (string_decoded.include? word) 
             puts letter
             puts string_decoded
         else
-            puts 'Shit'
+          #  puts 'Shit'
         end
      end
 end
